b6398264d83bd4dcee9df30c1a214cd95b724e53
jordonm <jordonmeril9@gmail.com>
2016-05-16 20:31:27 +0000

    lol

---
+++ scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function($) {
+
+  // 1. Include jQuery in your project
+
+  // 2. Select the elements you'll need.... $('...')
+
+  // 3. Write some JavaScript
+
+})(window.jQuery);
